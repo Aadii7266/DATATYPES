3332e6b948fcf3417f22f79f9798028b6360bb2a
Aadii7266 <adityaparihar7266@gmail.com>
2024-07-31 11:31:24 +0530

    Add files via upload

---
+++ DATATYPES.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int myIntVar;
+    float myFloatVar;
+    double myDoubleVar;
+    char myAlphabet;
+    string myLine;
+    bool decision;
+
+    cout << "Enter Integer" <<endl;
+    cin >> myIntVar;
+    cout << "Value of Variable is " << myIntVar << "Size is " << sizeof(myIntVar) <<endl;
